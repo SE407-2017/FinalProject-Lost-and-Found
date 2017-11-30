45801b22f4f8b0085468d765238687f953ba18f8
bohebuleng <32163437+bohebuleng@users.noreply.github.com>
2017-11-30 09:03:18 +0800

    添加意见箱 稍微改动一下

---
+++ lost_and_found添加意见箱 稍微改动一下/lost_and_found/users/models.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    nickname = models.CharField(max_length=50, blank=True)
+
+    class Meta(AbstractUser.Meta):
+        pass
+
+
+class Upload1(models.Model):
+	img = models.ImageField(upload_to = 'upload')
+        finder = models.CharField(max_length=10)
+        phoneNumber = models.CharField(max_length=10)
+        details = models.TextField(max_length=200)
+        user = models.ForeignKey(User, on_delete=models.CASCADE)
+        pub_date = models.DateTimeField('date published',auto_now_add=True)
+        index = -1
+
+class Suggestion(models.Model):
+    suggestion = models.TextField(max_length=200)
+    pub_date = models.DateTimeField('date published',auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    index = -1
